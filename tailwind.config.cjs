3b0a0b0655fad7b74164b3270f4bbd1a6193ab40
Jeda777 <jakub.jeda@wp.pl>
2022-11-06 22:40:11 +0100

    Starting point

---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.{html,css,scss}'],
+  theme: {
+    extend: {
+      colors: {
+        PaleViolet: '#d89eff',
+        ModerateViolet: '#9241c8',
+        DesaturatedDarkViolet: '#6e5d7e',
+        GrayishBlue: '#c6cacd',
+        VeryDarkDesaturatedViolet: '#3e2753',
+        DarkGrayishViolet: '#a39daa',
+        LightMagenta: '#e942ff',
+        LightViolet: '#8838ff',
+        LightGrayishViolet: '#f5f3f7',
+        VeryDarkDesaturatedViolet: '#3e2753',
+        VeryLightMagenta: '#e570ff',
+      },
+    },
+  },
+  plugins: [],
+}
